f15a660c35ac7f76754ea14c0430a0555ddb3e8e
asif is here <asifafran24@gmail.com>
2025-10-18 03:42:18 +0600

    robofriends update

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Card = (props) => {
+  const {name, email, id} = props;
+  return (
+    
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+        <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+        <div>
+          <h1>{name}</h1>
+          <p>{email}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Card; 
