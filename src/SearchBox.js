07d10c780f93d38708cbcb59aa333af149d9b58e
asif is here <asifafran24@gmail.com>
2025-10-19 00:58:39 +0600

    A responsive search field has been created

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchchange }) => {
     return(
         <div className="pa2">
             <input 
             className="pa3 ba b--green bg-lightest-blue" 
             type="search" 
             placeholder="search robots"
+            onChange={searchchange}
             />
         </div>
     )
